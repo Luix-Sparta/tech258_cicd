0f3562621f18070416efb6015393e8ba3ab7afda
Luix-Sparta <luixhiano@gmail.com>
2024-05-10 14:24:35 +0100

    Updated provision.sh

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -13,7 +13,9 @@ sudo apt-get install git -y
 # install nodejs
 sudo apt-get install python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs npm -y
+sudo apt-get install nodejs -y
+
+sudo npm install
 
 # install pm2
 sudo npm install pm2 -g
