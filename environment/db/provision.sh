5654334346a7facf917ddc4297f23975dda07094
Luix-Sparta <luixhiano@gmail.com>
2024-05-13 12:27:49 +0100

    Updated provision.sh

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -11,10 +11,9 @@ sudo apt-get upgrade -y
 # sudo apt-get install mongodb-org=3.2.20 -y
 sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
 
-# remoe the default .conf and replace with our configuration
-#sudo rm /etc/mongod.conf
-#sudo ln -s /home/ubuntu/environment/mongod.conf /etc/mongod.conf
-sudo cp /home/ubuntu/environment/mongod.conf /etc/mongod.conf
+# remove the default .conf and replace with our configuration
+sudo rm /etc/mongod.conf
+sudo ln -s /home/ubuntu/environment/mongod.conf /etc/mongod.conf
 
 # if mongo is is set up correctly these will be successful
 sudo systemctl restart mongod
