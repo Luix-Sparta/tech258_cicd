93e0beb6a18ad1ca4602fda7a151c910263874c4
Luix-Sparta <luixhiano@gmail.com>
2024-05-13 14:37:12 +0100

    Updated provsision1.sh

--- environment/db/provision1.sh
+++ environment/db/provision1.sh
@@ -10,11 +10,10 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
 
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get update
-
+sudo  apt-get update
 
 # sudo apt-get install mongodb-org=3.2.20 -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh=2.2.4 mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
+sudo apt-get install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh=2.2.4 mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
 
 # remove the default .conf and replace with our configuration
 sudo rm /etc/mongod.conf
@@ -25,5 +24,3 @@ sudo mv mongod.conf /etc/
 sudo systemctl enable mongod
 sudo systemctl restart mongod
 
-
-#test2
